0abee5e0cf98d42810468febdae1694204ba4251
zhuwencheng <zhuwencheng@ipaynow.cn>
2019-05-10 18:22:10 +0800

    1

--- src/permission.js
+++ src/permission.js
@@ -2,11 +2,13 @@ import router from './router'
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
 import { getToken } from '@/api/auth'
+import { Toast } from "mint-ui";
 
 NProgress.configure({ showSpinner: false })
 router.beforeEach((to, from, next) => {
   NProgress.start();
-  if (to.meta && to.meta.loginChecked && getToken()) {
+  if (to.meta && to.meta.loginChecked && !getToken()) {
+    Toast('请登录！');
     next(`/login?redirect=${to.path}`);
     // NProgress.done();
   } else {
